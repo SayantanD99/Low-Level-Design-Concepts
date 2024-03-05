f2c488ea6cdc64352a7d3ee264ed1de15c51634d
Sayantan Das <sayantandasbhp@gmail.com>
2024-03-05 12:53:12 +0530

    Merge Sort Using Threads

---
+++ src/Java_Basics/Threads/MergeSort/Main.java
@@ -0,0 +1,25 @@
+package Java_Basics.Threads.MergeSort;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        List<Integer> a = List.of(8,1,7,5,6,4,3,9);
+
+        ExecutorService es = Executors.newFixedThreadPool(10);
+        Sorter s = new Sorter(a, es);
+
+        Future<List<Integer>> ans = es.submit(s);
+        List<Integer> finalANs = ans.get();
+
+        for (Integer finalAN : finalANs) {
+            System.out.println(finalAN);
+        }
+
+
+    }
+}
