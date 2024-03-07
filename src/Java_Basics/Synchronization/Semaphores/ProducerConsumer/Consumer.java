c6929f4a58fb33c00b88e666543028fa9a848a1f
Sayantan Das <sayantandasbhp@gmail.com>
2024-03-07 10:30:31 +0530

    Semaphores

---
+++ src/Java_Basics/Synchronization/Semaphores/ProducerConsumer/Consumer.java
@@ -0,0 +1,21 @@
+package Java_Basics.Synchronization.Semaphores.ProducerConsumer;
+public class Consumer implements Runnable{
+
+    private Store s;
+
+    Consumer(Store s){
+        this.s = s;
+    }
+
+
+    @Override
+    public void run() {
+        while (true){
+            synchronized (s) {
+                if (s.canConsume()) {
+                    s.buy();
+                }
+            }
+        }
+    }
+}
---
+++ src/Java_Basics/Synchronization/Semaphores/ProducerConsumer_Semaphore/Consumer.java
@@ -0,0 +1,33 @@
+package Java_Basics.Synchronization.Semaphores.ProducerConsumer_Semaphore;
+
+import java.util.concurrent.Semaphore;
+
+public class Consumer implements Runnable{
+
+    private Store s;
+    private Semaphore pSema;
+    private Semaphore cSema;
+
+    Consumer(Store s, Semaphore producerSema,
+             Semaphore consumerSema) {
+        this.s = s;
+        pSema = producerSema;
+        cSema = consumerSema;
+
+    }
+    @Override
+    public void run() {
+        while (true){
+            try {
+                cSema.acquire();
+                Thread.sleep(400);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            s.buy();
+
+            pSema.release();
+
+        }
+    }
+}
