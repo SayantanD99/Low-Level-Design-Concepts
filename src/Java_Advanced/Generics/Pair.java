5bfd3ab5efcbf2c93f48d806345b9935eaaa84a0
Sayantan Das <sayantandasbhp@gmail.com>
2024-03-08 12:01:40 +0530

    Generics

---
+++ src/Java_Advanced/Generics/Pair.java
@@ -0,0 +1,44 @@
+package Java_Advanced.Generics;
+
+import Java_Basics.Inheritance.D;
+import java.util.List;
+
+public class Pair <V, T>{
+    V x;
+    T  y;
+
+
+    public void setX(V x) {
+        this.x = x;
+
+    }
+
+    public void setY(T y) {
+        this.y = y;
+    }
+
+
+    public T getY(){
+        return y;
+    }
+
+    public V getX() {
+        return x;
+    }
+
+    public  static <s,v> void doSomething(s x, v y){
+
+        System.out.println("Printing x: "+ x + " Y: "+ y);
+
+    }
+
+    public <s,v> void doSomething2(s x, v y){
+        System.out.println("Printing x: "+ x + " Y: "+ y);
+    }
+
+    public <A extends Number> double addition (A first, A second){
+        return first.doubleValue()+second.doubleValue();
+
+    }
+
+}
