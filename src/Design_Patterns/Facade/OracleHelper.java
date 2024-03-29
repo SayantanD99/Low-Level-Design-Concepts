40754ae684050308bc777da7d193baef5857338f
Sayantan Das <sayantandasbhp@gmail.com>
2024-03-29 11:09:14 +0530

    Facade Design Pattern

---
+++ src/Design_Patterns/Facade/OracleHelper.java
@@ -0,0 +1,19 @@
+package Design_Patterns.Facade;
+
+import java.sql.Connection;
+public class OracleHelper {
+
+    public static Connection getOracleDBConnection(){
+        //get Oracle DB connection using connection parameters
+        return null;
+    }
+
+    public void generateOraclePDFReport(String tableName, Connection con){
+        //get data from table and generate pdf report
+    }
+
+    public void generateOracleHTMLReport(String tableName, Connection con){
+        //get data from table and generate pdf report
+    }
+
+}
